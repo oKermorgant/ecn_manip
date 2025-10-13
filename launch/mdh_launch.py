140728ca4d59edc1d47c6f8af4b14edb7515ac3d
Olivier Kermorgant <olivier_kermorgant@yahoo.fr>
2025-10-13 16:03:22 +0200

    mdh launch

--- launch/mdh_launch.py
+++ launch/mdh_launch.py
@@ -81,7 +81,10 @@ class Joint:
         Mx = Homogeneous([self.a, 0, 0], [self.alpha, 0, 0])
         Mz = Homogeneous([0, 0, self.r], [0, 0, self.theta])
         M = np.dot(Mx,Mz)
-        R = Rotation.from_matrix(M[:3,:3])
+        try:
+            R = Rotation.from_matrix(M[:3,:3])
+        except:
+            R = Rotation.from_dcm(M[:3,:3])
 
         jtype = 'fixed'
         if self.dir:
